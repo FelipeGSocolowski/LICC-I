2d588247cf414571127a8cb5b3fa9d6b340af368
Felipe G. Socolowski <felipe.socolowski@gmail.com>
2025-04-22 23:49:02 -0300

    Adicionando as atividades 11 e 12

---
+++ Aula06/atividade11.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int tesouro;
+
+    scanf("%d", &tesouro);
+
+    printf("Valor inicial do tesouro: %d\n", tesouro);
+
+    int *mapa = &tesouro;
+
+    printf("Valor do tesouro usando mapa: %d\n", *mapa);
+
+    *mapa += 10;
+
+    printf("Valor do tesouro usando mapa depois de adicionarmos 10 moedas: %d\n", *mapa);
+
+    int **mapa2 = &mapa;
+
+    printf("Valor do tesouro usando mapa para o mapa: %d\n", **mapa2);
+
+    **mapa2 += 10;
+
+    printf("Valor do tesouro usando mapa para o mapa depois de adicionarmos 10 moedas: %d\n", **mapa2);
+}
---
+++ Aula06/atividade12.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int swap(int *num1, int *num2) {
+    int temp = *num1;
+
+    *num1 = *num2;
+
+    *num2 = temp;
+    
+    return 0;
+}
+
+int main() {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    int *ca = &a, *cb = &b;
+
+    swap(ca, cb);
+
+    printf("%d %d\n", a, b);
+}
